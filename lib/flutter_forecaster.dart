606308f36f73cc5f54864954f694f87785ce4a10
Ee You Sheng <62140209+EeYouSheng@users.noreply.github.com>
2025-03-19 17:17:14 +0800

    V1.0.1

---
+++ lib/flutter_forecaster.dart
@@ -0,0 +1,6 @@
+library;
+
+export 'src/rust/frb_generated.dart' show RustLib;
+export 'src/dart/arima_forecaster.dart';
+export 'src/dart/augur_forecaster.dart' ;
+export 'src/dart/model/prediction_source.dart' ;
