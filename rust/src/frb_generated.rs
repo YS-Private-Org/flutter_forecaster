f588ac0fd589344fb669e086ee9a503ea4463736
Ee You Sheng <62140209+EeYouSheng@users.noreply.github.com>
2025-07-07 19:14:49 +0800

    Match Version RustBridge with Codegen

--- rust/src/frb_generated.rs
+++ rust/src/frb_generated.rs
@@ -36,7 +36,7 @@ flutter_rust_bridge::frb_generated_boilerplate!(
     default_rust_opaque = RustOpaqueMoi,
     default_rust_auto_opaque = RustAutoOpaqueMoi,
 );
-pub(crate) const FLUTTER_RUST_BRIDGE_CODEGEN_VERSION: &str = "2.9.0";
+pub(crate) const FLUTTER_RUST_BRIDGE_CODEGEN_VERSION: &str = "2.10.0";
 pub(crate) const FLUTTER_RUST_BRIDGE_CODEGEN_CONTENT_HASH: i32 = 338048657;
 
 // Section: executor
